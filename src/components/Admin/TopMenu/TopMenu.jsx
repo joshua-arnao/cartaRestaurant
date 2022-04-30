d74ef5100e1126a8647c9f968845fb43c7dd6ead
joshua-arnao <joshua.arnao@icloud.com>
2022-04-29 19:55:18 -0500

    Logica del TopMenu(logout y nombre de usuario

--- src/components/Admin/TopMenu/TopMenu.jsx
+++ src/components/Admin/TopMenu/TopMenu.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import { MdLogout } from "react-icons/md";
-import { Flex, Spacer, Box, ButtonGroup, IconButton } from "@chakra-ui/react";
-
+import {
+  Grid,
+  Flex,
+  Spacer,
+  Box,
+  ButtonGroup,
+  IconButton,
+} from "@chakra-ui/react";
+import { useAuth } from "../../../hooks";
 import "./TopMenu.scss";
 
 export function TopMenu() {
+  const { auth, logout } = useAuth();
+
+  const renderName = () => {
+    if (auth.me?.first_name && auth.me?.last_name) {
+      return `${auth.me.first_name} ${auth.me.last_name}`;
+    }
+    return auth.me?.email;
+  };
+
   return (
-    <Box pos="fixed" w="100%" zIndex={1} boxShadow="base">
+    <Grid pos="fixed" w="100%" zIndex={1} boxShadow="base">
       <Flex minWidth="max-content" alignItems="center" gap="2" p="4">
         <Box>
-          <h1>Top Menu</h1>
+          <h1>Restaurante</h1>
         </Box>
         <Spacer />
         <ButtonGroup gap="2" alignItems="center">
           <Box>
-            <p>Hola, usuario...</p>
+            <p>Hola, {renderName()}</p>
           </Box>
           <Spacer />
           <IconButton
             icon={<MdLogout />}
             variant="outline"
             size="md"
+            onClick={logout}
           ></IconButton>
         </ButtonGroup>
       </Flex>
-    </Box>
+    </Grid>
   );
 }
